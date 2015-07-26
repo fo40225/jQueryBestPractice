b9a97b8f7ee6ed2ec751a0163ac5934db94ce315
fo40225 <fo40225@hotmail.com>
2015-07-26 20:32:19 +0800

    first commit

---
+++ jQueryBestPractice/Scripts/index.js
@@ -0,0 +1,24 @@
+﻿// IIFE - Immediately Invoked Function Expression
+(function (Main) {
+    // The global jQuery object is passed as a parameter
+    Main(window.jQuery, window, document);
+}(function ($, window, document) {
+    // The $ is now locally scoped
+
+    // The DOM may not be ready!;
+    // The code goes here!
+
+    // Listen for the jQuery ready event on the document
+    $(function () {
+        // The DOM is ready!
+        // The code goes here!
+
+    });
+
+    // Listen for the load event on the window
+    $(window).on("load", function () {
+        // Everything is ready!
+        // The code goes here!
+
+    });
+}));
